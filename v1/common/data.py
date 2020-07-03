cc169f4c2139b6bf7eb8ad1bf2c818be8f2684e5
Fabian Sandoval Saldias <fabianvss@gmail.com>
2020-07-03 21:49:04 +0200

    Add API v1 download script.

--- v1/common/data.py
+++ v1/common/data.py
@@ -7,12 +7,22 @@ from v1.detail.upgrade.v0 import upgrade_from_v0
 
 
 def _upgrade_data( data, error_on_noop ):
-    if 'api_version' in data:
-        if data['api_version'] != 1 or error_on_noop:
+    if not 'api_version' in data:
+        print( 'Upgrading data from v0 to v1...' )
+        return upgrade_from_v0( data )
+    version = data['api_version']
+    if version == 1:
+        if error_on_noop:
             print_err( 'Only data exported by v0 can be converted to v1.' )
             sys.exit( 1 )
-    print( 'Upgrading data from v0 to v1...' )
-    return upgrade_from_v0( data )
+        # TODO: complete upload script
+        print_err( 'Attention: v1 data cannot be processed yet, only downloaded.' )
+        sys.exit( 1 )
+        # return data
+    else:
+        print_err( f'Data version {version} not supported yet.' )
+        sys.exit( 1 )
+
 
 
 def load_data( filename, error_on_noop=False ):
---
+++ v1/download.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import json
+import os
+import sys
+
+import progress.bar
+import requests
+
+from all.common.auth import build_auth_header
+from all.common.utils import date_to_string
+from all.common.utils import simple_changeset_to_list
+from all.common.utils import verify_response
+from v1.common.auth import login
+from v1.common.auth import logout
+from v1.common.parser import add_default_arguments
+from v1.common.parser import verify_default_arguments
+
+
+def fetch_users( remote_data ):
+    r = requests.get( f'{remote_data.url}/user/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_subjects( remote_data ):
+    r = requests.get( f'{remote_data.url}/subject/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_locations( remote_data ):
+    r = requests.get( f'{remote_data.url}/location/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_activities( remote_data ):
+    r = requests.get( f'{remote_data.url}/activity/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_organizations( remote_data ):
+    r = requests.get( f'{remote_data.url}/organization/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_tracker_links( remote_data ):
+    r = requests.get( f'{remote_data.url}/tracker-link/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_tracker_projects( remote_data ):
+    r = requests.get( f'{remote_data.url}/tracker-project/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_tracker_issues( remote_data ):
+    r = requests.get( f'{remote_data.url}/tracker-issue/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_reports( remote_data ):
+    r = requests.get( f'{remote_data.url}/report/', headers=build_auth_header( remote_data.access_token ) )
+    verify_response( r )
+    return simple_changeset_to_list( r.json() )
+
+
+def fetch_data( remote_data ):
+    bar = progress.bar.Bar( f'Downloading...', max=9 )
+    users = fetch_users( remote_data )
+    bar.next()
+    subjects = fetch_subjects( remote_data )
+    bar.next()
+    locations = fetch_locations( remote_data )
+    bar.next()
+    activities = fetch_activities( remote_data )
+    bar.next()
+    organizations = fetch_organizations( remote_data )
+    bar.next()
+    tracker_links = fetch_tracker_links( remote_data )
+    bar.next()
+    tracker_projects = fetch_tracker_projects( remote_data )
+    bar.next()
+    tracker_issues = fetch_tracker_issues( remote_data )
+    bar.next()
+    reports = fetch_reports( remote_data )
+    bar.next()
+    bar.finish()
+    return {
+        'users': users,
+        'subjects': subjects,
+        'locations': locations,
+        'activities': activities,
+        'organizations': organizations,
+        'tracker_links': tracker_links,
+        'tracker_projects': tracker_projects,
+        'tracker_issues': tracker_issues,
+        'reports': reports,
+    }
+
+
+def export_data( remote_data, filename, skip_warning ):
+    data = {
+        'exported_on': date_to_string( datetime.datetime.utcnow() ),
+        'api_version': 1,
+        'user_id': remote_data.user_id,
+        'data': fetch_data( remote_data ),
+    }
+
+    if os.path.exists( filename ) and not skip_warning:
+        print( f'WARNING: {filename} already exists' )
+        print( f'         and will get overridden' )
+        input( 'Press Enter to continue' )
+    json.dump( data, open( filename, 'w' ), indent=4, sort_keys=False )
+
+
+def main():
+    parser = argparse.ArgumentParser( description='Export Beaverlog data.' )
+    add_default_arguments( parser, with_y=True )
+    parser.add_argument( 'output', metavar='OUTPUT', type=str, help='target json file' )
+
+    args = parser.parse_args()
+    verify_default_arguments( args )
+
+    remote_data = login( args.api, args.e, args.u, args.p )
+    try:
+        export_data( remote_data, args.output, args.y )
+    finally:
+        logout( remote_data )
+
+    print( 'Export successful.' )
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit( main() )
+    except KeyboardInterrupt:
+        sys.exit( 1 )
